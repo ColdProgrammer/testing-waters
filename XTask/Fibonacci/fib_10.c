237dc7f8d2633215c19d0f4c797c3242427c728f
coldprogrammer <timetoprogram@gmail.com>
2018-12-04 23:24:13 -0600

    untried

--- XTask/Fibonacci/fib_10.c
+++ XTask/Fibonacci/fib_10.c
@@ -14,18 +14,16 @@ long comp_fib_numbers(int n)
   // Basic algorithm: f(n) = f(n-1) + f(n-2)
   long fnm1, fnm2, fn;
   if ( n == 0 || n == 1 ) return(n);
-#pragma omp task shared(fnm1)
+
   fnm1 = comp_fib_numbers(n-1);
-#pragma omp task shared(fnm2)
   fnm2 = comp_fib_numbers(n-2);
-#pragma omp taskwait
   fn = fnm1 + fnm2;
   return(fn);
 }
 
 int main()
 {
-  int n=10;
+  int n=40;
   long result;
   /*if(argc<2){
     printf("usage ./fibonacci <number>\n");
@@ -33,9 +31,7 @@ int main()
   }
   n = atoi(argv[1]);*/
   //#pragma omp parallel num_threads(NUM_THREADS)
-#pragma omp parallel
   {
-#pragma omp single nowait
     {
       result = comp_fib_numbers(n);
     } // end of single region
--- XTask/Fibonacci/fib_20.c
+++ XTask/Fibonacci/fib_20.c
@@ -1,7 +1,4 @@
-/* File  : fibonacci.c
-* Computes fibonacci numbers
-* Last modified  : 2011-04-25
-*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +11,9 @@ long comp_fib_numbers(int n)
   // Basic algorithm: f(n) = f(n-1) + f(n-2)
   long fnm1, fnm2, fn;
   if ( n == 0 || n == 1 ) return(n);
-#pragma omp task shared(fnm1)
+#pragma omp task shared(fnm1) untied
   fnm1 = comp_fib_numbers(n-1);
-#pragma omp task shared(fnm2)
+#pragma omp task shared(fnm2) untied
   fnm2 = comp_fib_numbers(n-2);
 #pragma omp taskwait
   fn = fnm1 + fnm2;
@@ -25,7 +22,7 @@ long comp_fib_numbers(int n)
 
 int main()
 {
-  int n=20;
+  int n=30;
   long result;
   /*if(argc<2){
     printf("usage ./fibonacci <number>\n");
